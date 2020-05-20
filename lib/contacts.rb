354ff12387542224c58f133df879cc4e916d73bb
elsa chen <snowing222@gmail.com>
2020-05-20 19:18:57 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
     if person=="Freddy Mercury"
       contact_details_hash.each do |attribute,information|
         if attribute==:favorite_ice_cream_flavors
-          information.delete_if {|flavor| flavor=="strawberry"}
+          information.delete("strawberry")
         end
       end
     end
